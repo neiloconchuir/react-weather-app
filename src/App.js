abe27c82573a0e4b97667d79c03f93d07ba1db54
neiloconchuir <110842831+neiloconchuir@users.noreply.github.com>
2022-09-30 14:06:13 +0100

    Added github url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Neil O'Conchuir and is 
+        <a href="https://github.com/neiloconchuir/react-weather-app" target="_blank" rel="noreferrer">
+        open scource on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
