276f51a59cf9a8316e31ff1663acf41c7ed4d5b0
neiloconchuir <110842831+neiloconchuir@users.noreply.github.com>
2022-09-30 14:25:49 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Neil O'Conchuir and is 
-        <a href="https://github.com/neiloconchuir/react-weather-app" target="_blank" rel="noreferrer">
-        open scource on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Neil O'Conchuir and is{" "}
+          <a
+            href="https://github.com/neiloconchuir/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
