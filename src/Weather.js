5af016f3e872f0ed7924f642bf1c27b4e1bd5478
neiloconchuir <110842831+neiloconchuir@users.noreply.github.com>
2022-09-30 17:00:20 +0100

    Added axios and made first api call

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './Weather.css';
 
 export default function Weather() {
-  return (
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setTemperature(Math.round(response.data.main.temp));
+    setReady(true);
+  }
+
+  if (ready) {
+      return (
     <div className="Weather">
       <form>
         <div className="row">
@@ -34,7 +45,7 @@ export default function Weather() {
               src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
               alt="Mostly cloudy"             
             />
-            <span className="temperature">6</span>
+            <span className="temperature">{temperature}</span>
             <span className="unit">℃</span>         
         </div>
         <div className="col-6">
@@ -47,4 +58,12 @@ export default function Weather() {
       </div>
     </div>
   );
+  } else {
+    const apiKey = "6ad6478bca96e80d85e0658e8313cfc0";
+    let city = "New York";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
